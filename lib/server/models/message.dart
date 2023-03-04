43a89a1c97fbbe0ecc2b7b6ebec55d32eeaffd25
clover-leaf <thangnguyenmatt@gmail.com>
2023-03-04 10:54:46 +0700

    fix: reuse message enum

--- lib/server/models/message.dart
+++ lib/server/models/message.dart
@@ -1,20 +1,11 @@
-// enum Message {
-//   /// When client request menu from server
-//   /// i.e {'type': 'request_menu'}
-//   requestMenu('request_menu'),
+enum Message {
+  prepareOrder('prepare_order'),
 
-//   /// When kitchen update item in-stock status
-//   /// i.e {'type': 'update_item', 'item': Item.toJson}
-//   updateItem('update_item'),
+  deliveryOrder('delivery_order'),
 
-//   /// When client place new order
-//   /// i.e {
-//   /// 'type': 'place_order',
-//   /// 'order': Order.toJson
-//   /// }
-//   placeOrder('place_order');
+  reviewOrder('review_order');
 
-//   const Message(this.value);
+  const Message(this.value);
 
-//   final String value;
-// }
+  final String value;
+}
