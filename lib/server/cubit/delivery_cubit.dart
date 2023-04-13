3167438ecafe9617a4bf5e2a1e8393887a96d31c
clover-leaf <thangnguyenmatt@gmail.com>
2023-04-13 16:00:19 +0700

    fix: esp websocket

--- lib/server/cubit/delivery_cubit.dart
+++ lib/server/cubit/delivery_cubit.dart
@@ -4,8 +4,8 @@ class DeliveryCubit extends Cubit<List<String>> {
   // Create an instance with an initial state of empty String.
   DeliveryCubit() : super(<String>[]);
 
-  void delivery(List<String> deliveryDishID) {
-    emit(deliveryDishID);
+  void delivery(List<String> deliveryDishesID) {
+    emit(deliveryDishesID);
   }
 
   void reset() => emit(<String>[]);
--- routes/ws-order.dart
+++ routes/ws-order.dart
@@ -6,15 +6,16 @@ import 'package:pos_server/pos_server.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   final handler = webSocketHandler((channel, protocol) {
+    final deliveryCubit = context.read<DeliveryCubit>();
     final orderCubit = context.read<OrderCubit>()..subscribe(channel);
-    final espCubit = context.read<EspCubit>()..subscribe(channel);
+    final espCubit = context.read<EspCubit>();
 
-    // Listen for messages from the client.
     channel.stream.listen(
       (message) {
         final data = jsonDecode(message as String) as Map<String, dynamic>;
         if (data['type'] == Message.deliveryOrder.value) {
           espCubit.toTable(int.parse(data['table_id'] as String));
+          deliveryCubit.delivery(data['dishes_id'] as List<String>);
         }
         orderCubit.forwardMessage(message);
       },
