c4afcdd488a47e768320921356dd7ddde0759f8e
clover-leaf <thangnguyenmatt@gmail.com>
2023-04-27 10:20:39 +0700

    feat: add review

---
+++ routes/review.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:dart_frog/dart_frog.dart' as frog;
+import 'package:supabase/supabase.dart';
+
+// ?id=uuid&star=4
+Future<frog.Response> onRequest(frog.RequestContext context) async {
+  // Access the incoming request.
+  final request = context.request;
+
+  // Access the HTTP method.
+  final method = request.method.value;
+
+  if (method == frog.HttpMethod.get.value) {
+    final params = request.uri.queryParameters;
+
+    final dishId = params['id']!;
+    final star = int.parse(params['star']!);
+
+    final supabase = context.read<SupabaseClient>();
+
+    await supabase.from('review').insert({'dish_id': dishId, 'star': star});
+    return frog.Response(
+      body: jsonEncode({
+        'status': 'success',
+      }),
+    );
+  }
+
+  return frog.Response(statusCode: 404);
+}
